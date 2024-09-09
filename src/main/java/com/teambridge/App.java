c2518fe8d4cf6539e5c6663833d05815a6d73944
DennisSalmazs <skrn.sdet@gmail.com>
2024-09-09 11:29:00 +0100

    initial setup

--- src/main/java/com/teambridge/App.java
+++ src/main/java/com/teambridge/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DiscoveryServiceApplication {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryServiceApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 
 }
